6a423bd178c300d6726fbb900f35b0dd09cdca06
koerismo <56563658+koerismo@users.noreply.github.com>
2022-04-22 23:20:41 -0400

    More work

--- scripts/work.js
+++ scripts/work.js
@@ -1,5 +1,9 @@
 import './lib/jquery-3.6.0.min.js';
 
+
+/* ------------------ BASE LOADER LOGIC ------------------ */
+
+
 $('.subheader > span').on( 'click', function(){
 	if (this.classList.contains('active')) { return }
 	const category = this.innerText.toLowerCase();
@@ -10,28 +14,35 @@ $('.subheader > span').on( 'click', function(){
 	if (category in actions) {actions[category](this)}
 });
 
+
+// These actions fire whenever the tab is selected.
+const actions = {
+	environment: load_environment
+}
+
+
+/* ------------------ ENVIRONMENTS PAGE ------------------ */
+
+
 // The image cache is used to recall the images' dimensions.
 let environment_loaded = false;
 let cached_images = {};
 
-// These actions fire whenever the tab is selected.
-const actions = {
-	environment(){
-		if (environment_loaded) { return }
-		environment_loaded = true;
-		
-		Array.from($('div.body.environment div')).forEach((div)=>{
-			const url = div.getAttribute('data-url');
-			div.style.backgroundImage = `url(${url})`;
-			if (url in cached_images) { return div.classList.add('loaded') } // Quick optimization for duplicate images
-			cached_images[url] = new Image();
-			cached_images[url].onload = () => {div.classList.add('loaded')}
-			cached_images[url].src = url;
-		});
-	}
+function load_environment() {
+	if (environment_loaded) { return }
+	environment_loaded = true;
+	
+	Array.from($('div.body.environment div')).forEach((div)=>{
+		const url = div.getAttribute('data-url');
+		div.style.backgroundImage = `url(${url})`;
+		if (url in cached_images) { return div.classList.add('loaded') } // Quick optimization for duplicate images
+		cached_images[url] = new Image();
+		cached_images[url].onload = () => {div.classList.add('loaded')}
+		cached_images[url].src = url;
+	});
 }
 
-const viewer_bg = $('#viewer');
+const viewer_bg = $('#image-viewer');
 const viewer = viewer_bg.children()[0];
 
 $('div.body.environment div').on( 'click', function(){
@@ -44,7 +55,7 @@ $('div.body.environment div').on( 'click', function(){
 	const bbox = this.getBoundingClientRect();
 	const img_width = cached_images[url].width,
 		  img_height = cached_images[url].height;
-	const max_img_dim = Math.max(img_width, img_height),
+	const max_img_dim = Math.max( img_width, img_height ),
 		  min_win_dim = Math.min( $(window).width(), $(window).height());
 
 	// Apply initial transform
@@ -57,8 +68,8 @@ $('div.body.environment div').on( 'click', function(){
 
 	// Apply target transform with a slight delay
 	setTimeout(()=>{
-		let width_new = img_width / max_img_dim * (min_win_dim-50);
-		let height_new = img_height / max_img_dim * (min_win_dim-50);
+		const width_new = img_width / max_img_dim * (min_win_dim-50);
+		const height_new = img_height / max_img_dim * (min_win_dim-50);
 		viewer_bg.addClass('active');
 		viewer.style.backgroundImage = `url(${url})`;
 		viewer.style.height = height_new + 'px';
@@ -72,4 +83,108 @@ $('div.body.environment div').on( 'click', function(){
 viewer_bg.on( 'click', function(e){
 	viewer_bg.removeClass('active');
 	viewer_bg.addClass('hidden');
+});
+
+
+/* ------------------ AUDIO PAGE ------------------ */
+
+
+// Just a warning, this part is where the code gets *bad*.
+
+const audiop_el = $('div#audio-player'),
+	  audiop_input_el = $('div#audio-player input')[0],
+	  audiop_title_el = $('div#audio-player span#song-title')[0],
+	  audiop_position_el = $('div#audio-player span#song-position')[0],
+	  audiop_duration_el = $('div#audio-player span#song-duration')[0];
+
+
+let audio_currently_selected = null;
+let audio_currently_playing = null;
+let audio_last_playing = null;
+let audio_players = {};
+
+function audio_stop_playing() {
+	audiop_el.removeClass('active');
+	if (audio_currently_playing === null) { return }
+	audio_players[audio_currently_playing].onload = function(){ this.stop() }
+	audio_players[audio_currently_playing].pause();
+	audio_last_playing = audio_currently_playing;
+	audio_currently_playing = null;
+}
+function audio_begin_playing( url ) {
+
+	if (audio_currently_playing !== null) {
+		audio_stop_playing()
+	}
+
+	audio_currently_playing = url;
+	if (url in audio_players) {
+		initialize_audio_interface();
+		if (audio_last_playing !== url) { audio_players[url].currentTime = 0 }
+		return audio_players[url].play();
+	}
+
+	audio_players[url] = new Audio();
+	audio_players[url].onplay = () => {
+		// This is to handle the user pressing the keyboard pause button.
+		if (audio_currently_playing === null) {
+			audio_currently_selected.classList.add('active');
+			audio_currently_playing = url;
+		}
+		initialize_audio_interface();
+		audiop_el.addClass('active');
+	}
+	audio_players[url].onpause = () => { audiop_el.removeClass('active'); }
+	audio_players[url].autoplay = true;
+	audio_players[url].src = url;
+}
+
+function format_mmss( seconds ) {
+	const d = new Intl.DateTimeFormat('EN-US', {minute: 'numeric', second: 'numeric'});
+	return d.format( seconds*1000 );
+}
+
+function initialize_audio_interface() {
+	audio_players[audio_currently_playing].onended = () => {
+		audio_last_playing = null;
+		audio_currently_playing = null;
+		audiop_el.removeClass('active');
+		audio_currently_selected.classList.remove('active');
+	}
+
+	audiop_input_el.oninput = () => {
+		audio_players[audio_currently_playing].currentTime = audiop_input_el.value;
+	}
+
+	const duration = audio_players[audio_currently_playing].duration;
+	audiop_input_el.max = duration;
+	audiop_duration_el.innerText = format_mmss(duration);
+	update_audio_interface_interval();
+}
+
+function update_audio_interface_interval() {
+	if (audio_currently_playing === null) { return }
+	audiop_input_el.value = audio_players[audio_currently_playing].currentTime;
+	audiop_position_el.innerText = format_mmss(audio_players[audio_currently_playing].currentTime);
+	setTimeout( update_audio_interface_interval, 50 );
+}
+
+
+Array.from($('div.body.audio > div')).forEach( div => {
+	const title_span = document.createElement('span');
+	title_span.innerText = div.getAttribute('data-title').replaceAll('\\n','\n');
+	div.appendChild( title_span );
+
+	const audio_btn = document.createElement('a');
+	div.appendChild( audio_btn );
+});
+
+$('div.body.audio > div').on( 'click', function(){
+	const isPlaying = this.classList.contains('active');
+	$('div.body.audio > div').removeClass('active');
+	if (isPlaying) { return audio_stop_playing() }
+	audio_currently_selected = this;
+	this.classList.add('active');
+	audiop_title_el.innerText = this.getAttribute('data-title');
+	audio_begin_playing( this.getAttribute('data-url') );
 });
